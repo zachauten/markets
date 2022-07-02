b0945c2eab4dde5e305dd52f573e9ea700782161
Zach Auten <zachauten@gmail.com>
2022-07-02 18:48:27 -0400

    refactor css

--- src/client/index.ts
+++ src/client/index.ts
@@ -31,29 +31,141 @@ data.push( {name: "Example Farmer's Market", address: "Durham,NC", url: "http://
 data.push( {name: "Another Example Farmer's Market", address: "Durham,NC", url: "http://example.net"} );
 data.push( {name: "Test Farmer's Market", address: "Durham,NC", url: "http://example.net"} );
 
+const style = `
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    display: grid; 
+    grid-template-rows: auto 1fr auto; 
+
+    background-color: antiquewhite;
+
+    height: 100%;
+    width: 100%;
+
+    margin: 0px;
+    padding: 0px;
+  }
+
+  body h1 {
+    text-align: center;
+    font-size: xx-large;
+    font-weight: bold;
+  }
+
+  .tile-container {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+
+    padding: 50px;
+  }
+
+  .tile {
+    display: grid;
+    grid-template: 2fr 1fr / 1fr 1fr;
+
+    background-color: white;
+
+    border-style: outset;
+    border-radius: 0%;
+    box-shadow: 0 4px 8px 0 rgb(0 0 0 / 20%);
+
+    text-align: center;
+    height: 150px;
+    width: 300px;
+    margin: 20px;
+  }
+
+  .tile-button {
+    position: relative;
+
+    border-width: unset;
+    border-style: unset;
+    border-top-style: solid;
+    border-color: black;
+
+    background-color: unset;
+    padding: 0px;
+    cursor: pointer;
+    font: unset;
+  }
+
+  .tile-button:focus {
+    outline: blue 5px auto;
+  }
+
+  svg {
+    position: absolute;
+    color: black;
+    top: 50%;
+    left: 50%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);  
+  }
+
+  footer {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-content: center;
+    
+    height:3em;
+    width:100%;
+    bottom:0%;
+  }
+
+  .footer-item {
+    margin-right:10px;
+  }
+`;
+
 function renderBody() {
   return `
-  <body style="position:relative; height:100%; width:100%; margin: 0px; padding: 0px;">
-    <h1 style="text-align:center; font-size: xx-large; font-weight:bold;">NC Farmer's Markets</h1>
-    <div style="display: flex; flex-flow: row wrap; padding: 50px;">${data.map(x => renderTile(x)).join("")}</div>
+  <body>
+    <h1>NC Farmer's Markets</h1>
+    <div class="tile-container">${data.map(x => renderTile(x)).join("")}</div>
     ${renderFooter()}
   </body>
   `;
 }
 
 function renderFooter() {
-  return `<footer style="position:fixed; display:flex; flex-flow:row wrap; justify-content:center; align-content:center; background-color:lightgray; height:3em; width:100%; bottom:0%">
-      <span style="margin-right:10px;">By <a href="https://zach.sexy">zja</a></span>
-      <a style="margin-rigth:10px;"href="https://github.com/zachauten">Source</a>
+  return `<footer style="">
+      <span class="footer-item">By <a href="https://zach.sexy">zja</a></span>
+      <a class="footer-item" href="https://github.com/zachauten">Source</a>
   </footer>
   `;
 }
 
+// SVGs from https://tabler-icons.io/
 function renderTile(market: Market) {
-  return `<div style="position:relative; border-style: outset; box-shadow: 0 4px 8px 0 rgb(0 0 0 / 20%); text-align: center; height:150px; width:150px; margin:4px">
-  <a href="${market.url}">${market.name}</a>
-  <a style="position:absolute; font-size:xx-large; bottom: 0px; left: 0px; margin: 0px;" onclick="alert('download ics!')">📅</a>
-  <a style="position:absolute; font-size:xx-large; text-decoration: none; bottom:0px; right: 0px; margin: 0px;" href="http://maps.apple.com/?address=${market.address}">🗺</a>
+  return `<div class="tile">
+  <div style="grid-column: 1 / 3">
+    <a href="${market.url}">${market.name}</a>
+  </div>
+  <button class="tile-button" onclick="alert('foo')">
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-calendar-event" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <rect x="4" y="5" width="16" height="16" rx="2"></rect>
+    <line x1="16" y1="3" x2="16" y2="7"></line>
+    <line x1="8" y1="3" x2="8" y2="7"></line>
+    <line x1="4" y1="11" x2="20" y2="11"></line>
+    <rect x="8" y="15" width="2" height="2"></rect>
+    </svg>
+  </button>
+  <a class="tile-button" style="border-left-style: solid" href="http://maps.apple.com/?address=${market.address}">
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <line x1="18" y1="6" x2="18" y2="6.01"></line>
+    <path d="M18 13l-3.5 -5a4 4 0 1 1 7 0l-3.5 5"></path>
+    <polyline points="10.5 4.75 9 4 3 7 3 20 9 17 15 20 21 17 21 15"></polyline>
+    <line x1="9" y1="4" x2="9" y2="17"></line>
+    <line x1="15" y1="15" x2="15" y2="20"></line>
+    </svg>
+  </a>
 </div>`
 }
 
@@ -68,9 +180,7 @@ export function ssr() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>NC Farmer's Markets</title>
         <style>
-        * {
-          box-sizing: border-box;
-        }
+        ${style}
         </style>
       </head>
       ${body}
